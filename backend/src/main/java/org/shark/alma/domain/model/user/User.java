bdb28c43e58137f8e205a88592881b80aa66d1be
faguero <faguero@gmail.com>
2025-07-19 10:18:57 -0300

    Introduce `User` entity with attributes and annotations for JPA mapping

---
+++ backend/src/main/java/org/shark/alma/domain/model/user/User.java
@@ -0,0 +1,31 @@
+package org.shark.alma.domain.model.user;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "\"user\"", schema = "admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, default = "false")
+    private boolean administrador  ;
+
+}
